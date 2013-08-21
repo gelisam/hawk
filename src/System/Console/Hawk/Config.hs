b22d8e7bc225fe15dcd80b4d3ed1d389ab341de0
Samuel Gélineau <gelisam+github@gmail.com>
2013-08-21 08:24:44 -0400

    OverloadedStrings in the default prelude
    Since we import ByteString by default, OverloadedStrings makes sense.

--- src/System/Console/Hawk/Config.hs
+++ src/System/Console/Hawk/Config.hs
@@ -64,7 +64,7 @@ recompileConfigIfNeeded = do
     unless configFileExists $
         writeFile configFile $
             unlines
-            [ "{-# LANGUAGE ExtendedDefaultRules #-}"
+            [ "{-# LANGUAGE ExtendedDefaultRules, OverloadedStrings #-}"
             , "import Prelude"
             , "import qualified Data.ByteString.Lazy.Char8 as B"
             , "import qualified Data.List as L"]
