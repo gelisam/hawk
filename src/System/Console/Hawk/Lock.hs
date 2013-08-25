d24b2335026065422b00cdfb854a6485cb374da1
Samuel Gélineau <gelisam+github@gmail.com>
2013-08-25 16:29:39 -0400

    block, don't poll, while waiting for the lock

--- src/System/Console/Hawk/Lock.hs
+++ src/System/Console/Hawk/Lock.hs
@@ -1,11 +1,14 @@
 module System.Console.Hawk.Lock ( withLock ) where
 
-import Control.Concurrent ( threadDelay )
+--import Control.Concurrent ( threadDelay )
 import Control.Exception
 import Control.Monad ( guard )
 import GHC.IO.Exception
+import GHC.IO.Handle ( hGetContents, hClose )
 import Network.BSD ( getProtocolNumber ) -- still cross-platform, don't let the name fool you
+import Network ( PortID (..), connectTo )
 import Network.Socket
+import Text.Printf
 
 
 -- use a socket number as a lock indicator.
@@ -14,22 +17,47 @@ withLock body = withSocketsDo $ do
     bracket lock unlock $ const body
   where
     lock = do
-        catchJust isADDRINUSE open $ \() -> do
+        catchJust isADDRINUSE openSocket $ \() -> do
             -- open failed, the lock must be in use.
             
-            -- uncomment this and race two instances in order to verify that locking works
+            -- uncomment this and race two instances in order to verify that locking works.
             --putStrLn "** LOCKED **"
             
-            -- wait for the other instance to finish, then try again.
-            threadDelay 250000  -- 0.25 seconds
-            lock
-    unlock = close
+            -- uncomment and run less than 5 seconds before another process unlocks,
+            -- to verify that we correctly handle the case where the socket is closed
+            -- between openSocket and waitForException.
+            --threadDelay 5000000
+            
+            -- wait for the other instance to signal that it is done with the lock.
+            catchJust isDisconnected waitForException $ \() -> do
+              -- we were disconnected, the server must have released the lock!
+              
+              -- uncomment this and race with a long-running instance to verify
+              -- that we unlock as early as possible.
+              --putStrLn "** UNLOCKED **"
+              
+              -- try again.
+              lock
+    unlock = closeSocket
     
+    openHandle = connectTo "localhost" $ PortNumber portNumber
+    closeHandle = hClose
+    
+    waitForException :: IO a
+    waitForException = bracket openHandle closeHandle $ \h -> do
+      s <- hGetContents h
+      length s `seq` return ()  -- blocks until EOF, which never comes
+                                -- because the server never accepted the connection
+      error $ printf "port %s in use by a program other than hawk" $ show portNumber
     
     isADDRINUSE :: IOError -> Maybe ()
     isADDRINUSE = guard . (== "bind") . ioe_location
     
-    open = listenOn portNumber
+    isDisconnected :: IOError -> Maybe ()
+    isDisconnected = guard . (`elem` ["connect", "hGetContents"]) . ioe_location
+    
+    openSocket = listenOn portNumber
+    closeSocket = sClose
     
     -- the first few [0-9] characters of the sha1 of "hawk"
     portNumber :: PortNumber
