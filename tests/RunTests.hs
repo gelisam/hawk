1dd62c4ee1a6c39d34ef4f432837ef665b34b0e0
Samuel Gélineau <gelisam+github@gmail.com>
2014-01-05 17:46:31 -0500

    workaround for resurected ghc bug?
    Using the doctest library currently causes the tests to fail with the
    following message:
    
      reference: Too late for parseStaticFlags: call it before newSession
    
    There used to be an issue with that error message in an old version of
    ghc [1], not sure why we are triggering it again with a recent version.
    Regardless, the workaround is easy: use the executable version of
    doctest instead of the library version.
    
    [1] https://github.com/sol/doctest-haskell/issues/11

--- tests/RunTests.hs
+++ tests/RunTests.hs
@@ -12,6 +12,8 @@
 --   See the License for the specific language governing permissions and
 --   limitations under the License.
 
+import System.Exit
+import System.Process
 import Test.Framework
 --import Test.Framework.Providers.HUnit
 --import Test.Framework.Providers.QuickCheck2
@@ -33,4 +35,5 @@ main = do
         ReprTest.reprSpec
         ConfigTest.spec
     HawkTest.run
-    doctest ["-isrc", "tests/System/Console/Hawk/Lock/Test.hs"]
+    exitCode <- system "doctest -isrc tests/System/Console/Hawk/Lock/Test.hs"
+    exitWith exitCode
