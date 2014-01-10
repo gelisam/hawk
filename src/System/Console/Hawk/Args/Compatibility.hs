fb5314f725f3e3a97f95727ebd9dc20a4b2a2ee4
Samuel Gélineau <gelisam+github@gmail.com>
2014-01-09 19:43:04 -0500

    Spec, a more precise type for Hawk's arguments
    Not used yet.

---
+++ src/System/Console/Hawk/Args/Compatibility.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- | Convert new-style Specs to old-style Options.
+module System.Console.Hawk.Args.Compatibility where
+
+import System.Console.Hawk.Args.Spec
+import System.Console.Hawk.Options
+
+
+optionsFromSpec :: HawkSpec -> Options
+optionsFromSpec Help = defaultOptions { optHelp = True }
+optionsFromSpec Version = defaultOptions { optVersion = True }
+optionsFromSpec (Eval    x o) = updateExprOptions x
+                              $ updateOutputOptions o
+                              $ defaultOptions { optMode = EvalMode }
+optionsFromSpec (Apply i x o) = updateInputOptions i
+                              $ updateExprOptions x
+                              $ updateOutputOptions o
+                              $ defaultOptions { optMode = ApplyMode }
+optionsFromSpec (Map   i x o) = updateInputOptions i
+                              $ updateExprOptions x
+                              $ updateOutputOptions o
+                              $ defaultOptions { optMode = MapMode }
+
+updateInputOptions :: InputSpec -> Options -> Options
+updateInputOptions (InputSpec _ format) = go format
+  where
+    go RawStream            o = o { optLinesDelim = Just "" }
+    go (Lines s lineFormat) o = goLine lineFormat
+                              $ o { optLinesDelim = Just s }
+    goLine RawLine   o = o { optWordsDelim = Just "" }
+    goLine (Words s) o = o { optWordsDelim = Just s }
+
+updateOutputOptions :: OutputSpec -> Options -> Options
+updateOutputOptions (OutputSpec _ format) = go format
+  where
+    go (OutputFormat lineD wordD) o = o { optOutLinesDelim = Just lineD
+                                        , optOutWordsDelim = Just wordD
+                                        }
+
+updateExprOptions :: ExprSpec -> Options -> Options
+updateExprOptions (ExprSpec p _) = updatePreludeOptions p
+
+updatePreludeOptions :: PreludeSpec -> Options -> Options
+updatePreludeOptions UseUserPrelude     o = o { optRecompile = True }
+updatePreludeOptions UseCachedPrelude   _ = error "cannot be represented with Options"
+updatePreludeOptions (UsePreludeFile f) o = o { optModuleFile = Just f }
+updatePreludeOptions DetectPrelude      o = o
---
+++ src/System/Console/Hawk/Args/Spec.hs
@@ -0,0 +1,74 @@
+-- | The precisely-typed version of Hawk's command-line arguments.
+module System.Console.Hawk.Args.Spec where
+
+import Data.ByteString (ByteString)
+
+
+data HawkSpec
+    = Help
+    | Version
+    | Eval            ExprSpec OutputSpec
+    | Apply InputSpec ExprSpec OutputSpec
+    | Map   InputSpec ExprSpec OutputSpec
+  deriving (Show, Eq)
+
+
+data InputSpec = InputSpec
+    { inputSource :: InputSource
+    , inputFormat :: InputFormat
+    }
+  deriving (Show, Eq)
+
+data OutputSpec = OutputSpec
+    { outputSink :: OutputSink
+    , outputFormat :: OutputFormat
+    }
+  deriving (Show, Eq)
+
+
+data InputSource
+    = UseStdin
+    | InputFile FilePath
+  deriving (Show, Eq)
+
+data OutputSink
+    = UseStdout
+    -- | OutputFile FilePath  -- we might want to implement --in-place
+                              -- in the future
+  deriving (Show, Eq)
+
+data InputFormat
+    = RawStream
+    | Lines Separator LineFormat
+  deriving (Show, Eq)
+
+data LineFormat
+    = RawLine
+    | Words Separator
+  deriving (Show, Eq)
+
+-- We can't know ahead of time whether it's going to be a raw stream
+-- or raw lines or words, it depends on the type of the user expression.
+data OutputFormat = OutputFormat
+    { lineDelimiter :: Separator
+    , wordDelimiter :: Separator
+    }
+  deriving (Show, Eq)
+
+type Separator = ByteString
+
+
+data ExprSpec = ExprSpec
+    { userPrelude :: PreludeSpec
+    , userExpression :: String
+    }
+  deriving (Show, Eq)
+
+data PreludeSpec
+    = UseUserPrelude
+    | UseCachedPrelude
+    | UsePreludeFile FilePath
+    -- | UsePreludeText String  -- we might want to accept prelude literals
+                                -- in the future, to simplify testing
+    | DetectPrelude  -- cached if possible, else user
+  deriving (Show, Eq)
