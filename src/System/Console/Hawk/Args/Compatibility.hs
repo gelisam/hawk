c5021dbd1fb91e615e7bf515955b4bbfc8d62a8e
Samuel Gélineau <gelisam+github@gmail.com>
2014-01-16 07:19:45 -0500

    extract the 'not-options'

--- src/System/Console/Hawk/Args/Compatibility.hs
+++ src/System/Console/Hawk/Args/Compatibility.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 -- | Convert new-style Specs to old-style Options.
-module System.Console.Hawk.Args.Compatibility where
+module System.Console.Hawk.Args.Compatibility
+  ( optionsFromSpec
+  , notOptionsFromSpec
+  ) where
+
+import Control.Applicative
+import Data.Maybe
 
 import System.Console.Hawk.Args.Spec
 import System.Console.Hawk.Options
@@ -45,3 +51,28 @@ updatePreludeOptions UseUserPrelude     o = o { optRecompile = True }
 updatePreludeOptions UseCachedPrelude   _ = error "cannot be represented with Options"
 updatePreludeOptions (UsePreludeFile f) o = o { optModuleFile = Just f }
 updatePreludeOptions DetectPrelude      o = o
+
+
+-- | The "not option"s are the extra string arguments after all the flags.
+--   In the case of Hawk, this is the user expression and optionally an input
+--   file.
+notOptionsFromSpec :: HawkSpec -> [String]
+notOptionsFromSpec s = maybeToList expr ++ maybeToList file
+  where
+    expr = userExpression <$> exprSpecFromSpec s
+    file = do
+        i <- inputSpecFromSpec s
+        case inputSource i of
+          InputFile f -> return f
+          _           -> Nothing
+
+exprSpecFromSpec :: HawkSpec -> Maybe ExprSpec
+exprSpecFromSpec (Eval  e   _) = Just e
+exprSpecFromSpec (Apply e _ _) = Just e
+exprSpecFromSpec (Map   e _ _) = Just e
+exprSpecFromSpec _ = Nothing
+
+inputSpecFromSpec :: HawkSpec -> Maybe InputSpec
+inputSpecFromSpec (Apply _ i _) = Just i
+inputSpecFromSpec (Map   _ i _) = Just i
+inputSpecFromSpec _ = Nothing
