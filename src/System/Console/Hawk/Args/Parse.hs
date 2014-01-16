586bd125f46094c35fd006f970b8de465c165a64
Samuel Gélineau <gelisam+github@gmail.com>
2014-01-15 20:16:40 -0500

    add tests to Args.Parse

--- src/System/Console/Hawk/Args/Parse.hs
+++ src/System/Console/Hawk/Args/Parse.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, PackageImports #-}
 -- | In which Hawk's command-line arguments are structured into a `HawkSpec`.
 module System.Console.Hawk.Args.Parse (parseArgs) where
 
 import Control.Applicative
+import "mtl" Control.Monad.Trans
 
 import Control.Monad.Trans.OptionParser
 import Control.Monad.Trans.Uncertain
@@ -10,18 +11,63 @@ import qualified System.Console.Hawk.Args.Option as Option
 import           System.Console.Hawk.Args.Option (HawkOption, options)
 import System.Console.Hawk.Args.Spec
 
+-- $setup
+-- >>> let testP parser = runUncertainIO . runOptionParserT options parser
+
 
 -- | (line delimiter, word delimiter)
 type CommonDelimiters = (Separator, Separator)
 
-commonDelimiters :: OptionParser HawkOption CommonDelimiters
+-- | Extract '-D' and '-d'. We perform this step separately because those two
+--   delimiters are used by both the input and output specs.
+-- 
+-- >>> let test = testP commonDelimiters
+-- 
+-- >>> test []
+-- ("\n"," ")
+-- 
+-- >>> test ["-D\\n", "-d\\t"]
+-- ("\n","\t")
+-- 
+-- >>> test ["-D|", "-d,"]
+-- ("|",",")
+commonDelimiters :: (Functor m, Monad m)
+                 => OptionParserT HawkOption m CommonDelimiters
 commonDelimiters = do
     l <- consumeLast Option.LineDelimiter "\n" Option.consumeDelimiter
     w <- consumeLast Option.WordDelimiter " " Option.consumeDelimiter
     return (l, w)
 
 
-inputSpec :: CommonDelimiters -> OptionParser HawkOption InputSpec
+-- | The input delimiters have already been parsed, but we still need to
+--   interpret them and to determine the input source.
+-- 
+-- >>> :{
+-- let test = testP $ do { c <- commonDelimiters
+--                       ; _ <- consumeExtra consumeString  -- skip expr
+--                       ; i <- inputSpec c
+--                       ; lift $ print $ inputSource i
+--                       ; lift $ print $ inputFormat i
+--                       }
+-- :}
+-- 
+-- >>> test []
+-- UseStdin
+-- Lines "\n" (Words " ")
+-- 
+-- >>> test ["-d", "-a", "L.reverse"]
+-- UseStdin
+-- Lines "\n" RawLine
+-- 
+-- >>> test ["-D", "-a", "B.reverse"]
+-- UseStdin
+-- RawStream
+-- 
+-- >>> test ["-d:", "-m", "L.head", "/etc/passwd"]
+-- InputFile "/etc/passwd"
+-- Lines "\n" (Words ":")
+inputSpec :: (Functor m, Monad m)
+          => CommonDelimiters -> OptionParserT HawkOption m InputSpec
 inputSpec (l, w) = InputSpec <$> source <*> format
   where
     source = do
@@ -35,7 +81,31 @@ inputSpec (l, w) = InputSpec <$> source <*> format
     lineFormat | w == ""   = RawLine
                | otherwise = Words w
 
-outputSpec :: CommonDelimiters -> OptionParser HawkOption OutputSpec
+-- | The output delimiters take priority over the input delimiters, regardless
+--   of the order in which they appear.
+-- 
+-- >>> :{
+-- let test = testP $ do { c <- commonDelimiters
+--                       ; o <- outputSpec c
+--                       ; let OutputFormat l w = outputFormat o
+--                       ; lift $ print $ outputSink o
+--                       ; lift $ print (l, w)
+--                       }
+-- :}
+-- 
+-- >>> test []
+-- UseStdout
+-- ("\n"," ")
+-- 
+-- >>> test ["-D;", "-d", "-a", "L.reverse"]
+-- UseStdout
+-- (";","")
+-- 
+-- >>> test ["-o\t", "-d,", "-O|"]
+-- UseStdout
+-- ("|","\t")
+outputSpec :: (Functor m, Monad m)
+           => CommonDelimiters -> OptionParserT HawkOption m OutputSpec
 outputSpec (l, w) = OutputSpec <$> sink <*> format
   where
     sink = return UseStdout
@@ -44,7 +114,30 @@ outputSpec (l, w) = OutputSpec <$> sink <*> format
     word = consumeLast Option.OutputWordDelimiter w Option.consumeDelimiter
 
 
-exprSpec :: OptionParser HawkOption ExprSpec
+-- | The information we need in order to evaluate a user expression:
+--   the expression itself, and the context in which it should be evaluated.
+--   In Hawk, that context is the user prelude.
+-- 
+-- >>> :{
+-- let test = testP $ do { e <- exprSpec
+--                       ; lift $ print $ userExpression e
+--                       ; lift $ print $ userPrelude e
+--                       }
+-- :}
+-- 
+-- >>> test []
+-- error: missing user expression
+-- *** Exception: ExitFailure 1
+-- 
+-- >>> test ["-D;", "-d", "-a", "L.reverse"]
+-- "L.reverse"
+-- DetectPrelude
+-- 
+-- >>> test ["-r", "-m", "L.head", "file.in"]
+-- "L.head"
+-- UseUserPrelude
+exprSpec :: (Functor m, Monad m)
+         => OptionParserT HawkOption m ExprSpec
 exprSpec = ExprSpec <$> prelude <*> expr
   where
     prelude = do
@@ -57,18 +150,43 @@ exprSpec = ExprSpec <$> prelude <*> expr
           Nothing -> fail "missing user expression"
 
 
--- |
--- >>> runUncertain $ parseArgs ["--help"]
+-- | Parse command-line arguments to construct a `HawkSpec`.
+-- 
+-- TODO: complain if some arguments are unused (except perhaps "-d" and "-D").
+-- 
+-- >>> :{
+-- let test args = do { spec <- runUncertain $ parseArgs args
+--                    ; case spec of
+--                        Help        -> putStrLn "Help"
+--                        Version     -> putStrLn "Version"
+--                        Eval  e   o -> putStrLn "Eval"  >> print (userExpression e)                                         >> print (lineDelimiter (outputFormat o), wordDelimiter (outputFormat o))
+--                        Apply e i o -> putStrLn "Apply" >> print (userExpression e, inputSource i) >> print (inputFormat i) >> print (lineDelimiter (outputFormat o), wordDelimiter (outputFormat o))
+--                        Map   e i o -> putStrLn "Map"   >> print (userExpression e, inputSource i) >> print (inputFormat i) >> print (lineDelimiter (outputFormat o), wordDelimiter (outputFormat o))
+--                    }
+-- :}
+-- 
+-- >>> test ["--help"]
 -- Help
 -- 
--- >>> runUncertain $ parseArgs ["--version"]
+-- >>> test ["--version"]
 -- Version
 -- 
--- >>> runUncertain $ parseArgs ["-d\\t", "L.head"]
--- Eval (ExprSpec {userPrelude = DetectPrelude, userExpression = "L.head"}) (OutputSpec {outputSink = UseStdout, outputFormat = OutputFormat {lineDelimiter = "\n", wordDelimiter = "\t"}})
+-- >>> test ["-d\\t", "L.head"]
+-- Eval
+-- "L.head"
+-- ("\n","\t")
 -- 
--- >>> runUncertain $ parseArgs ["-d\\t", "-m", "L.head"]
--- Map (ExprSpec {userPrelude = DetectPrelude, userExpression = "L.head"}) (InputSpec {inputSource = UseStdin, inputFormat = Lines "\n" (Words "\t")}) (OutputSpec {outputSink = UseStdout, outputFormat = OutputFormat {lineDelimiter = "\n", wordDelimiter = "\t"}})
+-- >>> test ["-D\r\n", "-d\\t", "-m", "L.head"]
+-- Map
+-- ("L.head",UseStdin)
+-- Lines "\r\n" (Words "\t")
+-- ("\r\n","\t")
+-- 
+-- >>> test ["-D", "-O\n", "-m", "L.head", "file.in"]
+-- Map
+-- ("L.head",InputFile "file.in")
+-- RawStream
+-- ("\n"," ")
 parseArgs :: [String] -> Uncertain HawkSpec
 parseArgs = runOptionParserT options $ do
     cmd <- consumeExclusive assoc eval
