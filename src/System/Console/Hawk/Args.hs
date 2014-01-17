d5a40b5b9d0694b09276e71ea45812714039c311
Samuel Gélineau <gelisam+github@gmail.com>
2014-01-16 20:05:23 -0500

    add missing file
    should have been included in 83c16588a9965b920bad511920417aa824ef1080

---
+++ src/System/Console/Hawk/Args.hs
@@ -0,0 +1,16 @@
+-- | Re-export the most important functions from Args.*
+module System.Console.Hawk.Args
+  ( HawkSpec(..)
+  , InputSpec(..), OutputSpec(..)
+  , InputSource(..), OutputSink(..)
+  , InputFormat(..), LineFormat(..)
+  , OutputFormat(..), Separator
+  , ExprSpec(..), PreludeSpec(..)
+  , parseArgs
+  , optionsFromSpec
+  , notOptionsFromSpec
+  ) where
+
+import System.Console.Hawk.Args.Compatibility
+import System.Console.Hawk.Args.Parse
+import System.Console.Hawk.Args.Spec
