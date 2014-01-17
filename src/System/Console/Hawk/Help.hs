344c391e8f65bc3b23e39df14f0084ff5453d600
Samuel Gélineau <gelisam+github@gmail.com>
2014-01-16 22:41:05 -0500

    restructuring Hawk.Help

--- src/System/Console/Hawk/Help.hs
+++ src/System/Console/Hawk/Help.hs
@@ -9,24 +9,24 @@ import System.Console.GetOpt
 import System.Environment
 import System.Exit
 import System.IO
+import Text.Printf
 
 import System.Console.Hawk.Options
 
 
-getUsage :: IO String
-getUsage = do
-    pn <- getProgName
-    return $ usageInfo ("Usage: " ++ pn ++ " [<options>] <expr> [<file>]") 
-                       options
-
 hPrintUsage :: Handle -> IO ()
 hPrintUsage h = do
-    usage <- getUsage
+    hawk <- getProgName
+    let header = printf "Usage: %s [options] <expr> [<file>]" hawk
+    let usage = usageInfo header options
     hPutStr h usage
+
 
 help :: IO ()
 help = hPrintUsage stdout
 
+-- | A version of `help` which prints the usage to stderr, after printing a
+--   custom error message.
 failHelp :: String -> IO ()
 failHelp msg = do
     hPutStrLn stderr msg
