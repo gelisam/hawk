2568d47f7d0b9a0e8c028a1c81f01f23b916d830
Samuel Gélineau <gelisam+github@gmail.com>
2014-03-01 17:20:43 -0500

    extract the interpreter wrapper

---
+++ src/System/Console/Hawk/Interpreter.hs
@@ -0,0 +1,88 @@
+-- | A wrapper around the hint library, specialized for Hawk usage.
+module System.Console.Hawk.Interpreter where
+
+import Control.Monad
+import qualified Data.List as L
+import Data.List ((++))
+import Data.Either
+import Data.Function
+import Data.Maybe
+import Data.String
+import qualified Data.Typeable.Internal as Typeable
+import Data.Typeable.Internal
+  (TypeRep(..)
+  ,tyConName)
+import Language.Haskell.Interpreter
+import qualified Prelude as P
+import qualified System.IO as IO
+import System.IO (IO)
+import Text.Printf (printf)
+
+import Control.Monad.Trans.Uncertain
+import System.Console.Hawk.Args
+import System.Console.Hawk.Args.Spec
+import qualified System.Console.Hawk.Context as Context
+import System.Console.Hawk.Context.Compatibility
+import System.Console.Hawk.Sandbox
+import System.Console.Hawk.UserPrelude
+import System.Console.Hawk.Help
+import System.Console.Hawk.Lock
+import System.Console.Hawk.Runtime.Base
+import System.Console.Hawk.Version
+
+
+-- | Tell hint to load the user prelude, the modules it imports, and the
+--   language extensions it specifies.
+initInterpreter :: (String, String) -- ^ prelude file and module name
+                -> [(String,Maybe String)] -- ^ the modules maybe qualified
+                -> [Extension]
+                -> InterpreterT IO ()
+initInterpreter (preludeFile,preludeModule) userModules extensions = do
+        
+        set [languageExtensions := extensions]
+
+        -- load the prelude file
+        loadModules [preludeFile]
+
+        -- load the prelude module plus representable
+        setImportsQ $ (preludeModule,Nothing):defaultModules
+                                           ++ userModules
+
+
+errorString :: InterpreterError -> String
+errorString (WontCompile es) = L.intercalate "\n" (header : P.map indent es)
+  where
+    header = "Won't compile:"
+    indent (GhcError e) = ('\t':e)
+errorString e = P.show e
+
+wrapErrorsM :: Monad m => m (Either InterpreterError a) -> UncertainT m a
+wrapErrorsM = lift >=> wrapErrors
+
+wrapErrors :: Monad m => Either InterpreterError a -> UncertainT m a
+wrapErrors (Left e) = fail $ errorString e
+wrapErrors (Right x) = return x
+
+
+runLockedHawkInterpreter :: InterpreterT IO a -> IO (Either InterpreterError a)
+runLockedHawkInterpreter i = withLock $ runHawkInterpreter i
+
+-- | Wrapper used to force `typeOf` to fully-qualify the type
+--   `HawkRuntime`. Otherwise hint may try to use a type which
+--   we haven't explicitly imported.
+-- 
+-- >>> let runtime = HawkRuntime defaultInputSpec defaultOutputSpec
+-- 
+-- >>> Typeable.typeOf runtime
+-- HawkRuntime
+-- 
+-- >>> Typeable.typeOf $ QR runtime
+-- System.Console.Hawk.Runtime.Base.HawkRuntime
+newtype QualifiedHawkRuntime = QR HawkRuntime
+
+instance Typeable.Typeable QualifiedHawkRuntime where
+  typeOf (QR bs) = let TypeRep fp tc trs = Typeable.typeOf bs
+                   in TypeRep fp
+                              tc{ tyConName = "System.Console.Hawk.Runtime.Base."
+                                          ++ tyConName tc }
+                              trs
