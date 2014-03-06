bbafb0b045ea1c07b6b59a60a55dd3cf2267da74
Samuel Gélineau <gelisam+github@gmail.com>
2014-03-05 23:14:02 -0500

    chunk-constructors
    The whole point of this data structure was to be able to easily describe
    a divider between two chunks. Now it really is easy.

--- src/Data/Chunks.hs
+++ src/Data/Chunks.hs
@@ -3,7 +3,8 @@ module Data.Chunks where
 
 import Data.Monoid
 import Data.Foldable
-import Data.Sequence as S
+import Data.Sequence hiding (empty, singleton)
+import qualified Data.Sequence as S
 
 
 -- | Isomorphic to [[a]], only the instances are different.
@@ -15,8 +16,21 @@ fromLists = StickyList . fromList
 -- |
 -- >>> toLists (fromLists ["foo", "bar"] <> fromLists ["baz", "quux"])
 -- ["foo","barbaz","quux"]
+-- 
+-- >>> toLists (singleton "foo" <> empty <> singleton "bar" <> divider <> singleton "baz" <> singleton "quux")
+-- ["foobar","bazquux"]
 toLists :: Chunks a -> [[a]]
 toLists = toList . runStickyList
+
+
+empty :: StickyList a
+empty = StickyList S.empty
+
+singleton :: a -> StickyList a
+singleton = StickyList . S.singleton
+
+divider :: Monoid a => StickyList a
+divider = StickyList (S.singleton mempty >< S.singleton mempty)
 
 
 -- | Generalize Chunks to arbitrary monoids.
@@ -26,7 +40,7 @@ toLists = toList . runStickyList
 newtype StickyList w = StickyList { runStickyList :: Seq w }
 
 instance Monoid w => Monoid (StickyList w) where
-  mempty = StickyList empty
+  mempty = StickyList S.empty
   mappend (StickyList xs') (StickyList ys') | S.null xs' = StickyList ys'
   mappend (StickyList xs') (StickyList ys') | S.null ys' = StickyList xs'
   mappend (StickyList xs') (StickyList ys') = StickyList zs'
@@ -34,4 +48,4 @@ instance Monoid w => Monoid (StickyList w) where
       (xs :> x) = viewr xs'
       (y :< ys) = viewl ys'
       z = x `mappend` y
-      zs' = xs >< singleton z >< ys
+      zs' = xs >< S.singleton z >< ys
