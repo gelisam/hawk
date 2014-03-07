80e491b1357762567d3a01482b71c3db8673434c
Samuel Gélineau <gelisam+github@gmail.com>
2014-03-06 22:27:12 -0500

    ignore space at the both ends of lines

--- src/Data/HaskellSource/Parse.hs
+++ src/Data/HaskellSource/Parse.hs
@@ -9,6 +9,7 @@ import "mtl" Control.Monad.Trans
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Writer
 import qualified Data.ByteString.Char8 as B
+import Data.Char
 import Data.Functor.Identity
 
 import Data.Chunks
@@ -43,7 +44,8 @@ testP str = go . runWriterT . (`runStateT` source)
         mapM_ show_line xs
         putStrLn "==="
 
--- |
+-- | End of file.
+-- 
 -- >>> testP "foo" eof
 -- Nothing
 -- 
@@ -54,7 +56,8 @@ eof = do
     [] <- get
     return ()
 
--- |
+-- | Consume a line.
+-- 
 -- >>> testP "foo\nbar\n" line
 -- "foo"
 -- "foo"
@@ -78,6 +81,17 @@ line = do
       Left s -> return s
       Right s -> return (B.pack s)
 
+-- | Consume a line, stripping the whitespace at both ends.
+-- >>> testP "  hello  " stripped_line
+-- "  hello  "
+-- "hello"
+stripped_line :: SourceParser B.ByteString
+stripped_line = strip <$> line
+  where
+    strip = dropWhileEnd isSpace
+          . B.dropWhile isSpace
+    dropWhileEnd p = fst . B.spanEnd p 
+
 -- |
 -- >>> testP "foo\nbar\nbaz\n" (line >> line >> next_chunk >> line)
 -- "foo"
@@ -94,28 +108,28 @@ next_chunk = lift $ tell $ divider
 -- ["-- single line comment"]
 -- 
 -- >>> testP "{- multi\n   line\n   comment -}\n" comment
--- "{- multi"
--- "   line"
--- "   comment -}"
+-- "{- multi      "
+-- "   line       "
+-- "   comment -} "
 -- ["{- multi","   line","   comment -}"]
 comment :: SourceParser [B.ByteString]
 comment = single_line_comment <|> multi_line_comment
   where
     single_line_comment = do
-        x <- line
+        x <- stripped_line
         guard ("--" `B.isPrefixOf` x)
         return [x]
     
     -- nested comments not supported
     multi_line_comment = do
-        x <- line
+        x <- stripped_line
         guard ("{-" `B.isPrefixOf` x)
         xs <- inside_comment x
         return (x:xs)
     
-    inside_comment s | "-}" `B.isInfixOf` s = return []
+    inside_comment s | "-}" `B.isSuffixOf` s = return []
     inside_comment s | otherwise = do
-        x <- line
+        x <- stripped_line
         xs <- inside_comment x
         return (x:xs)
 
@@ -127,19 +141,19 @@ comment = single_line_comment <|> multi_line_comment
 -- 
 -- >>> testP "module Foo\n  ( main\n  ) where\nmain = 42\n" module_declaration
 -- "module Foo"
--- "  ( main"
--- "  ) where"
+-- "  ( main  "
+-- "  ) where "
 -- ["module Foo","  ( main","  ) where"]
 -- "main = 42"
 module_declaration :: SourceParser [B.ByteString]
 module_declaration = do
-    x <- line
+    x <- stripped_line
     guard ("module " `B.isPrefixOf` x)
     xs <- inside_declaration x
     return (x:xs)
   where
-    inside_declaration s | " where" `B.isInfixOf` s = return []
+    inside_declaration s | " where" `B.isSuffixOf` s = return []
     inside_declaration s | otherwise = do
-        x <- line
+        x <- stripped_line
         xs <- inside_declaration x
         return (x:xs)
