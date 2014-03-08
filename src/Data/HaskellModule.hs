248912407ae82d9bba173a275b240cce39de7a5a
Samuel Gélineau <gelisam+github@gmail.com>
2014-03-08 14:12:47 -0500

    backtrack - drop the parser combinators
    I thought I could get away with a few simple parser combinators because
    I don't need to recover the full AST of the user prelude, I only need to
    divide the user prelude into four sections. But it turns out there are
    many details such as nested comments and multi-line import lists which
    would require more subtle parser combinators. So I will try a different
    approach based on haskell-src-exts's SrcLoc annotations.

--- src/Data/HaskellModule.hs
+++ src/Data/HaskellModule.hs
@@ -7,7 +7,6 @@ import Data.ByteString.Char8 as B
 import Text.Printf
 
 import Data.HaskellSource
-import Data.HaskellSource.Parse
 
 
 -- | hint has `Interpreter.Extension`, but strings are simpler.
@@ -52,26 +51,3 @@ addImport qm m = m
         printf "import %s" fullName
     importStatement (fullName, Just qualifiedName) =
         printf "import qualified %s as %s" fullName qualifiedName
-
-
-parseModule :: B.ByteString -> Maybe HaskellModule
-parseModule = haskellModule . splitSource fourChunks . parseSource
-  where
-    haskellModule (Just [pS, mS, iS, cS]) = Just $ HaskellModule
-      { languageExtensions = []   , pragmaSource = pS
-      , moduleName = Nothing      , moduleSource = mS
-      , importedModules = []      , importSource = iS
-                                  , codeSource   = cS
-      }
-    haskellModule _ = Nothing
-    
-    fourChunks :: SourceParser ()
-    fourChunks = do
-        commentLines >> next_chunk
-        moduleLine   >> next_chunk
-        importLines  >> next_chunk
-        remainingLines
-    commentLines = (comment >> commentLines) <|> return ()
-    moduleLine = module_declaration <|> return ()
-    importLines = (import_declaration >> importLines) <|> return ()
-    remainingLines = (line >> remainingLines) <|> eof
--- tests/RunTests.hs
+++ tests/RunTests.hs
@@ -26,9 +26,7 @@ main :: IO ()
 main = do
     doctest' "tests/System/Console/Hawk/Lock/Test.hs"
     doctest' "src/Data/Cache.hs"
-    doctest' "src/Data/Chunks.hs"
     doctest' "src/Data/HaskellSource.hs"
-    doctest' "src/Data/HaskellSource/Parse.hs"
     doctest' "src/System/Console/Hawk.hs"
     doctest' "tests/System/Console/Hawk/PreludeTests.hs"
     doctest' "src/System/Console/Hawk/Args/Option.hs"
