a2cb071834604fcffd1abc8e54be99b87e175477
Samuel Gélineau <gelisam+github@gmail.com>
2014-03-08 11:20:07 -0500

    first pass at ParseModule using parser combinators
    We split the source into three sections of interest: the language
    pragmas, the optional module declaration, and the imports.
    
    No information is extracted from these sections yet; I will probably
    delegate to haskell-src-ext.

--- src/Data/HaskellModule.hs
+++ src/Data/HaskellModule.hs
@@ -2,9 +2,12 @@
 -- | A version of HaskellSource with slightly more semantics.
 module Data.HaskellModule where
 
+import Control.Applicative
+import Data.ByteString.Char8 as B
 import Text.Printf
 
 import Data.HaskellSource
+import Data.HaskellSource.Parse
 
 
 -- | hint has `Interpreter.Extension`, but strings are simpler.
@@ -49,3 +52,26 @@ addImport qm m = m
         printf "import %s" fullName
     importStatement (fullName, Just qualifiedName) =
         printf "import qualified %s as %s" fullName qualifiedName
+
+
+parseModule :: B.ByteString -> Maybe HaskellModule
+parseModule = haskellModule . splitSource fourChunks . parseSource
+  where
+    haskellModule (Just [pS, mS, iS, cS]) = Just $ HaskellModule
+      { languageExtensions = []   , pragmaSource = pS
+      , moduleName = Nothing      , moduleSource = mS
+      , importedModules = []      , importSource = iS
+                                  , codeSource   = cS
+      }
+    haskellModule _ = Nothing
+    
+    fourChunks :: SourceParser ()
+    fourChunks = do
+        commentLines >> next_chunk
+        moduleLine   >> next_chunk
+        importLines  >> next_chunk
+        remainingLines
+    commentLines = (comment >> commentLines) <|> return ()
+    moduleLine = module_declaration <|> return ()
+    importLines = (import_declaration >> importLines) <|> return ()
+    remainingLines = (line >> remainingLines) <|> eof
