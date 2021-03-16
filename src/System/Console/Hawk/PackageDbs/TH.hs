45989755bdd6c94cc3fde7b1f58e2710a12b2d78
Samuel Gélineau <gelisam+github@gmail.com>
2021-03-15 23:35:03 -0400

    support both pre and post template-haskell-2.17

--- src/System/Console/Hawk/PackageDbs/TH.hs
+++ src/System/Console/Hawk/PackageDbs/TH.hs
@@ -1,21 +1,32 @@
+{-# LANGUAGE CPP #-}
 module System.Console.Hawk.PackageDbs.TH
     ( compileTimeEnvVar
     , compileTimeWorkingDirectory
     ) where
 
 import Language.Haskell.TH.Syntax (TExp(TExp), Q, lift, runIO)
+#if MIN_VERSION_template_haskell(2,17,0)
+import Language.Haskell.TH.Syntax (Code, liftCode)
+#endif
 import System.Directory (getCurrentDirectory)
 import System.Environment (getEnvironment)
 
 
-compileTimeEnvVar :: String -> Q (TExp (Maybe String))
-compileTimeEnvVar varname = do
+#if !MIN_VERSION_template_haskell(2,17,0)
+type Code m a = m (TExp a)
+
+liftCode :: m (TExp a) -> Code m a
+liftCode = id
+#endif
+
+compileTimeEnvVar :: String -> Code Q (Maybe String)
+compileTimeEnvVar varname = liftCode $ do
   env <- runIO getEnvironment
   let r :: Maybe String
       r = lookup varname env
   TExp <$> lift r
 
-compileTimeWorkingDirectory :: Q (TExp String)
-compileTimeWorkingDirectory = do
+compileTimeWorkingDirectory :: Code Q String
+compileTimeWorkingDirectory = liftCode $ do
   pwd <- runIO getCurrentDirectory
   TExp <$> lift pwd
